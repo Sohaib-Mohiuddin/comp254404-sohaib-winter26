90fbebbae61a5e841377a360205682de122c58d5
sohaib <sohaibm131@gmail.com>
2026-01-22 22:45:51 +0000

    Organized Lab 1

--- sohaib_lab1/src/ex1/SinglyLinkedList.java
+++ sohaib_lab1/src/ex1/SinglyLinkedList.java
@@ -20,7 +20,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.opsdevelop.teaching.Lab2Exercises.linkedlists;
+package ex1;
 
 /**
  * A basic singly linked list implementation.
--- sohaib_lab1/src/ex2/DoublyLinkedList.java
+++ sohaib_lab1/src/ex2/DoublyLinkedList.java
@@ -20,7 +20,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.opsdevelop.teaching.Lab2Exercises.linkedlists;
+package ex2;
 
 /**
  * A basic doubly linked list implementation.
